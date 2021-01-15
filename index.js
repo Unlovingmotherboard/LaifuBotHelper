99b93458e0ec944f28edd3a75b6d30d894b083c9
Unlovingmotherboard <jmhern25@yahoo.com>
2021-01-15 00:37:38 -0600

    adding changes

--- index.js
+++ index.js
@@ -10,16 +10,72 @@ bot.on('ready', () => {
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+  if (msg.content === 'b.allen') {
+    msg.channel.send('@everyone Allen is a big doo doo head');
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
-  }
+  } 
+
+  if (msg.content === 'l.list') {
+
+    const filter = (m) => m.author.id === '688202466315206661';
+
+    msg.channel.awaitMessages(filter, {
+      max: 1,
+      time: 70000,
+      errors:['time']
+    })
+    .then((collected) => {
+
+      const collectedMsg = collected.first();
+
+
+      if(collectedMsg.embeds[0].description === "Lootcode has spawned! Enter the code using the `claim` command to win goodies!"){
+        msg.channel.send("OOPS! LOOTCRATE SPAWNED. TRY L.LIST AGAIN!");
+      }
+
+      bot.on('messageUpdate', (oldMessage, newMessage) => {
+
+        let loggedWaifuList = oldMessage;
+
+
+        if(newMessage.author.id === '688202466315206661') {
+          console.log("1");
+
+          console.log(`
+        
+
+        `);
+        console.log("----PREVIOUS PAGE----");
+        console.log(oldMessage.embeds[0].description);
+        console.log("------------------------------");
+        console.log(`
+      
+        `);
+
+        console.log("----NEXT PAGE----");
+        console.log(newMessage.embeds[0].description);
+        console.log("------------------------------");
+        console.log(`
+        
+        
+        
+
+
+        
+        `);
+
+        console.log("2");
+        }
+      });
+      
+
+      //THIS SENDS OUT THE LIST FROM MESSAGE  msg.channel.send(collectedMsg.embeds[0].description); 
+      
+    })
+    .catch(err => {
+      msg.channel.send("Something went wrong...");
+      console.log(err);
+    });
+
+  } 
 });
